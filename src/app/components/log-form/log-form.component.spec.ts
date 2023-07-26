f207b1da7e1bc531139d987dd40ba987aad61e8f
swwayam <codingswayam24@gmail.com>
2023-07-26 10:04:12 +0530

    initial commit

---
+++ src/app/components/log-form/log-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogFormComponent } from './log-form.component';
+
+describe('LogFormComponent', () => {
+  let component: LogFormComponent;
+  let fixture: ComponentFixture<LogFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LogFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
